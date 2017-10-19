b10cd62af7fcb341adcd56ac2200fd63e047bb94
Diego Dantas <diego.dantas@centralar.com.br>
2017-10-19 15:12:51 -0200

    a

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ require('dotenv').config({silent: true});
 
 var server = require('./app');
 //var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
-
 var port = process.env.PORT || 8080;
 
 server.listen(port, function() {
